8fe9f8298ea8ead395df8878b6bd31df0cdf8713
aolim45 <limabs.AL@gmail.com>
2024-10-18 16:13:48 -0500

    3371 Assignment 2 Javascript

---
+++ 3371as2js.js
@@ -0,0 +1,11 @@
+/*
+Name: Abigail Lim
+File: homework2.js
+Date created: 2024-10-18
+Date last edited: 2024-10-19
+Purpose: To validate & redisplay all of the user's inputs.
+*/
+
+/* Grabs date from user system */
+document.getElementById("currentDate").innerHTML = new Date(),toLocaleDateString();
+console.log(currentDate);
